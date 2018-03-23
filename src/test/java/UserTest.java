f92ba3854ce8d4f32498346387e92fe46fbb179f
Maxime <maximedelange1991@hotmail.com>
2018-03-23 11:30:57 +0100

    Email is null test and build should pass

--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -48,6 +48,6 @@ public class UserTest {
     public void testErrorOnWrongEmail(){
         User user = new User("Maxime", null);
         
-        assertNotNull(user.getEmail());
+        assertNull(user.getEmail());
     }
 }
